4705d5f330a82a42a216b722f45dcd5fa0028699
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-08-22 12:08:13 -0300

    chore: carregar modulo de exportacao

--- app/graficos.js
+++ app/graficos.js
@@ -1,7 +1,7 @@
-/* MoveMaster — carregador: gráficos do painel + notificações (toasts). */
+/* MoveMaster — carregador: gráficos do painel + notificações (toasts) + exportação. */
 (function () {
   'use strict';
-  ['graficos-core.js', 'toasts.js'].forEach(function (arquivo) {
+  ['graficos-core.js', 'toasts.js', 'exportar.js'].forEach(function (arquivo) {
     var s = document.createElement('script');
     s.src = arquivo;
     s.async = false;
