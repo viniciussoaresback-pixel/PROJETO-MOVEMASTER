d365fed4add6f64d8262683ea598e95cf3a98e00
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-08-27 09:52:23 -0300

    carregador: incluir microinteracoes.css

--- app/graficos.js
+++ app/graficos.js
@@ -1,7 +1,7 @@
-/* MoveMaster — carregador: gráficos do painel + notificações (toasts) + exportação + tema claro + refinamento 8. */
+/* MoveMaster — carregador: gráficos do painel + notificações (toasts) + exportação + tema claro + refinamento 8 + micro-interações. */
 (function () {
   'use strict';
-  ['tema-claro.css', 'refinamento-8.css'].forEach(function (css) {
+  ['tema-claro.css', 'refinamento-8.css', 'microinteracoes.css'].forEach(function (css) {
     var l = document.createElement('link');
     l.rel = 'stylesheet';
     l.href = css;
