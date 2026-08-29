e1d7c409b3c7c4d0cc03a97a254c31f148e87db8
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-08-28 21:06:01 -0300

    Update service worker version to movemaster-v257

--- app/sw.js
+++ app/sw.js
@@ -8,7 +8,7 @@
    login e uploads precisam ser sempre ao vivo.
    ===================================================================== */
 
-const VERSAO = 'movemaster-v256';
+const VERSAO = 'movemaster-v257';
 
 // Arquivos do "esqueleto" do app, guardados para funcionar offline
 const ARQUIVOS_BASE = [
