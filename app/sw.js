2102491044d2a25fcd6cdb7c2d2f3f3d878b4996
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-09-12 14:43:07 -0300

    Update service worker version to movemaster-v392

--- app/sw.js
+++ app/sw.js
@@ -8,7 +8,7 @@
    login e uploads precisam ser sempre ao vivo.
    ===================================================================== */
 
-const VERSAO = 'movemaster-v391';
+const VERSAO = 'movemaster-v392';
 
 // Arquivos do "esqueleto" do app, guardados para funcionar offline
 const ARQUIVOS_BASE = [
