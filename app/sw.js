00e66b15028591edb3af5fb0a6bdeaf6c1f327a6
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-09-17 08:41:13 -0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -8,7 +8,7 @@
    login e uploads precisam ser sempre ao vivo.
    ===================================================================== */
 
-const VERSAO = 'movemaster-v436';
+const VERSAO = 'movemaster-v437';
 
 // Arquivos do "esqueleto" do app, guardados para funcionar offline
 const ARQUIVOS_BASE = [
